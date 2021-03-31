fb764670ec8b499402a38571adbbb807995a3280
azseza <azeer.ltifi@gmail.com>
2021-04-01 00:52:22 +0100

    maf

---
+++ backmeup.sh
@@ -0,0 +1,220 @@
+#!/usr/bin/bash
+
+function print_version() {
+    echo "Auteurs : "
+    echo "      ___         ___         ___                   ___         ___         ___  ___         ___                   ___         ___         ___         ___         ___         ___         ___     "
+    echo "     /\  \       /\  \       /\__\                 /\  \       /\  \       /\__\/\  \       /\__\                 /\__\       /\  \       /\__\       /\  \       /\__\       /\  \       /\  \    "
+    echo "    /::\  \     /::\  \     /::|  |               /::\  \     /::\  \     /:/  /::\  \     /:/  /                /::|  |     /::\  \     /:/  /      /::\  \     /::|  |     /::\  \     /::\  \   "
+    echo "   /:/\:\  \   /:/\:\  \   /:|:|  |              /:/\ \  \   /:/\:\  \   /:/  /:/\:\  \   /:/__/                /:|:|  |    /:/\:\  \   /:/__/      /:/\:\  \   /:|:|  |    /:/\:\  \   /:/\:\  \  "
+    echo "  /::\~\:\__\ /::\~\:\  \ /:/|:|  |__           _\:\~\ \  \ /::\~\:\  \ /:/  /::\~\:\  \ /::\  \ ___           /:/|:|__|__ /:/  \:\  \ /::\  \ ___ /::\~\:\  \ /:/|:|__|__ /::\~\:\  \ /:/  \:\__\ "
+    echo " /:/\:\ \:|__/:/\:\ \:\__/:/ |:| /\__\         /\ \:\ \ \__/:/\:\ \:\__/:/__/:/\:\ \:\__/:/\:\  /\__\         /:/ |::::\__/:/__/ \:\__/:/\:\  /\__/:/\:\ \:\__/:/ |::::\__/:/\:\ \:\__/:/__/ \:|__|"
+    echo " \:\~\:\/:/  \:\~\:\ \/__\/__|:|/:/  /         \:\ \:\ \/__\/__\:\/:/  \:\  \/__\:\/:/  \/__\:\/:/  /         \/__/~~/:/  \:\  \ /:/  \/__\:\/:/  \/__\:\/:/  \/__/~~/:/  \:\~\:\ \/__\:\  \ /:/  /"
+    echo "  \:\ \::/  / \:\ \:\__\     |:/:/  /           \:\ \:\__\      \::/  / \:\  \   \::/  /     \::/  /                /:/  / \:\  /:/  /     \::/  /     \::/  /      /:/  / \:\ \:\__\  \:\  /:/  / "
+    echo "   \:\/:/  /   \:\ \/__/     |::/  /             \:\/:/  /      /:/  /   \:\  \  /:/  /      /:/  /                /:/  /   \:\/:/  /      /:/  /      /:/  /      /:/  /   \:\ \/__/   \:\/:/  /  "
+    echo "    \::/__/     \:\__\       /:/  /               \::/  /      /:/  /     \:\__\/:/  /      /:/  /                /:/  /     \::/  /      /:/  /      /:/  /      /:/  /     \:\__\      \::/__/   "
+    echo "     ~~          \/__/       \/__/                 \/__/       \/__/       \/__/\/__/       \/__/                 \/__/       \/__/       \/__/       \/__/       \/__/       \/__/       ~~       "
+    echo "          ___      ___ "    
+    echo "     /\  \    /\  \    "
+    echo "    /::\  \   \:\  \   "
+    echo "   /:/\:\  \   \:\  \  "
+    echo "  /::\~\:\  \  /::\  \ "
+    echo " /:/\:\ \:\__\/:/\:\__"
+    echo " \:\~\:\ \/__/:/  \/__/"
+    echo "  \:\ \:\__\/:/  /     "
+    echo "   \:\ \/__/\/__/      "
+    echo "    \:\__\             "
+    echo "     \/__/             "
+    echo " █     █░▄▄▄       ██████  ██████ ██▓███▄ ▄███▓    ██░ ██ ██▓██▓   ▄▄▄      ██▓    ██▓"
+    echo "▓█░ █ ░█▒████▄   ▒██    ▒▒██    ▒▓██▓██▒▀█▀ ██▒   ▓██░ ██▓██▓██▒  ▒████▄   ▓██▒   ▓██▒"
+    echo "▒█░ █ ░█▒██  ▀█▄ ░ ▓██▄  ░ ▓██▄  ▒██▓██    ▓██░   ▒██▀▀██▒██▒██░  ▒██  ▀█▄ ▒██░   ▒██▒"
+    echo "░█░ █ ░█░██▄▄▄▄██  ▒   ██▒ ▒   ██░██▒██    ▒██    ░▓█ ░██░██▒██░  ░██▄▄▄▄██▒██░   ░██░"
+    echo "░░██▒██▓ ▓█   ▓██▒██████▒▒██████▒░██▒██▒   ░██▒   ░▓█▒░██░██░██████▓█   ▓██░██████░██░"
+    echo "░ ▓░▒ ▒  ▒▒   ▓▒█▒ ▒▓▒ ▒ ▒ ▒▓▒ ▒ ░▓ ░ ▒░   ░  ░    ▒ ░░▒░░▓ ░ ▒░▓  ▒▒   ▓▒█░ ▒░▓  ░▓  "
+    echo "  ▒ ░ ░   ▒   ▒▒ ░ ░▒  ░ ░ ░▒  ░ ░▒ ░  ░      ░    ▒ ░▒░ ░▒ ░ ░ ▒  ░▒   ▒▒ ░ ░ ▒  ░▒ ░"
+    echo "  ░   ░   ░   ▒  ░  ░  ░ ░  ░  ░  ▒ ░      ░       ░  ░░ ░▒ ░ ░ ░   ░   ▒    ░ ░   ▒ ░"
+    echo "    ░         ░  ░     ░       ░  ░        ░       ░  ░  ░░     ░  ░    ░  ░   ░  ░░  "
+    echo "                                                                                      "
+
+}
+
+function help() {  
+echo "oooooooooo                        oooo       ooooo  oooo                   ooooooo8                                                           o8                        "
+echo " 888    888   ooooooo    ooooooo   888  ooooo 888    88 ooooooooo        o888    88   ooooooooo8 oo oooooo   ooooooooo8 oo oooooo   ooooooo o888oo ooooooo  oo oooooo   "
+echo " 888oooo88    ooooo888 888     888 888o888    888    88  888    888      888    oooo 888oooooo8   888   888 888oooooo8   888    888 ooooo888 888 888     888 888    888 "
+echo " 888    888 888    888 888         8888 88o   888    88  888    888      888o    88  888          888   888 888          888      888    888 888 888     888 888        "
+echo "o888ooo888   88ooo88 8o  88ooo888 o888o o888o  888oo88   888ooo88         888ooo888    88oooo888 o888o o888o  88oooo888 o888o      88ooo88 8o 888o 88ooo88  o888o       "
+echo "                                                   o888                                                                                                            "
+echo "Un outil qui vous aide a effectuer un back up de vos fishiers "
+echo "~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~"
+echo ""
+echo "USAGE : "
+echo ""
+echo "backmeup [OPTION] [OPTION] {file}"
+echo "OPTIONS : "
+echo "          -c : Copier tout les fishiers dans le repertoire courrant dans /backup/$USER."
+echo "          -r : Compresser tout les fishiers dans le repertoire /backup/$USER."
+echo "          -x : Compresser les fishiers dans le repertoire backup , ou bien les recompresser."
+echo "          -t : Restorer les fishiers compresseés."
+echo "          -m : Interactive mode."
+echo "          -o FILE : Afficher tout les fishiers compressées dans FILE."
+echo "          -f PATTERN : voir si PATTERN existe dans FILE."
+echo "          -h / -help : Afficher se message d'aide"
+
+}
+function c_copy() {
+    #copier tou ce qu'il y a dans le repertoire backup
+    cp * /backup/$USER
+}
+
+function r_archiver() {
+    #créer une backup sinon supprimer 
+    ls /backup/$USER | grep -c > $var
+    if [[ $var == 0  ]]; then
+        tar cfz /back/$USER.tar /backup/$USER
+    else
+        pwd > $ret
+        cd /backup
+        find . -type f -not -name '*.tar'-delete
+        cd $ret
+        tar cfz /backup/$USER.tar /backup/$USER
+    fi
+}
+
+function x_comprees() {
+    grep /backup/$USER.tar -c > $var
+    if [[ $var != 0 ]]; then
+        xz /backup/$USER.tar 
+        rm -f /backup/$USER.tar
+    else 
+        echo "FISHIER *.tar  n'existe pas "
+    fi
+}
+
+function t_restore() {
+    #restorer les fishiers dans $HOME
+    grep /backup/$USER.tar -c > $flagtar
+    grep /backup/$USER.tar.xz -c > $flagxz
+    if [[ $flagtar -eq 0 ]]; then 
+        tar -xvf /backup/$HOME.tar 
+    fi
+    if [[ $flagxz -eq 0 ]]; then 
+        xz --decompress /backup/$HOME.tar.xz
+    fi
+}
+
+#function g_yadInterface(){
+#
+#}
+#
+#function interac(){
+#
+#}
+
+function o_listErrIfNotExists() {
+    tar --list --verbose --file=archive.tar > $o_file
+    exit 1
+}
+
+function f_flagaaagjaj(){
+    cat $o_file | grep $pattern
+    exit 1
+}
+
+function s_sauvgarder() {
+    ls -arl > $file_s
+
+}
+while  getopts "crxthgmvs:o:f:" arg; do
+    case "$arg" in
+        -c)
+            shift
+            c_copy
+            exit 1
+            shift
+            ;;
+        -r)
+            shift
+            r_archiver
+            exit 1
+            shift
+            ;;
+        -x)
+            shift
+            x_comprees
+            exit 1
+            shift
+            ;;
+        -t)
+            shift
+            t_restore
+            exit 1
+            shift
+            ;;
+        #-g)
+        #    shift
+        #    g_flag = true
+        #    shift
+        #    ;;
+        #-m)
+        #    shift
+        #    interactiveMode
+        #    exit 1
+        #    shift
+        #    ;;
+        -s)
+            shift
+            $file_s = $arg
+            s_sauvgarder
+            exit 1
+            shift
+            ;;
+        -o)
+            shift
+            o_flag = 'true'
+            grep $arg -c > $var1
+            if [['$var' == '1']];then
+                $o_file = $arg 
+                o_listErrIfNotExists
+            else 
+                help
+                exit 1 
+            fi
+            shift
+            ;;
+        -f)
+            shift
+            if [[ o_flag == 'true' ]]; then
+                $pattern = $arg  
+            else
+                help
+                exit 1
+            fi 
+            shift
+            ;;
+        -help)
+            shift
+            help
+            exit 1
+            shift
+            ;;
+        -h)
+            shift
+            help
+            exit 1
+            shift
+            ;;
+        -v)
+            shift
+            print_version
+            exit 1 
+            shift
+            ;;
+        *)
+           shift
+           echo "$1 n'est pas une option reconnue !"
+           help
+           exit 1
+           shift
+           ;;
+    esac
+done
